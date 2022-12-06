544403897ede4688aa8684c00c065be4fa1e4ed9
Celia Maginot <celia.maginot@orange.fr>
2022-12-06 06:35:36 +0100

    advent_of_code_2022 - 06 done

---
+++ 06/06-01/main.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+static bool is_marker(std::vector<char> v)
+{
+	for (std::vector<char>::const_iterator it=v.begin(); it!=v.end(); ++it)
+	{
+		std::vector<char>::const_iterator it_bis=it;
+		for (++it_bis; it_bis!=v.end(); ++it_bis)
+		{
+			if (*it == *it_bis || *it == '\0' || *it_bis == '\0')
+				return (false);
+		}
+	}
+	return (true);
+}
+
+int main()
+{
+	int n_char = 0;
+	std::vector<char> v(4, '\0');
+	std::ifstream input("input.txt");
+	std::string line;
+
+	while(std::getline(input, line))
+	{
+		for (std::string::const_iterator it=line.begin(); it!=line.end(); ++it)
+		{
+			n_char++;
+			v.erase(v.begin());
+			v.push_back(*it);
+			if (is_marker(v) == true)
+			{
+				break ;
+				break ;
+			}
+		}
+	}
+
+	std::cout << "number of characters need to be processed before the first start-of-packet marker is detected : ";
+	std::cout << n_char << std::endl;
+	return (0);
+}
