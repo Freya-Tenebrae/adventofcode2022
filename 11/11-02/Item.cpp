2467c18d2438c3f075a42adcfa2c1220a4218651
Celia Maginot <celia.maginot@orange.fr>
2022-12-11 08:53:33 +0100

    advent_of_code_2022 - 11 small update

--- 11/11-02/Item.cpp
+++ 11/11-02/Item.cpp
@@ -12,25 +12,22 @@ Item::~Item()
 
 bool	Item::is_div(int modulo)
 {
-	int act_val = _base_value, tmp_value;
+	int act_val = _base_value;
 
 	std::vector<char>::iterator it_op = _op.begin();
 	for (std::vector<int>::iterator it_v_op = _v_op.begin(); \
 		it_v_op != _v_op.end(); it_v_op++)
 	{
 		act_val = act_val % modulo;
-		tmp_value = *it_v_op % modulo;
-
 		if (*it_op == '+')
-			act_val += tmp_value;
+			act_val += *it_v_op % modulo;
 		else if (*it_op == '*')
 		{
 			if (*it_v_op == 0)
 				act_val *= act_val;
 			else
-				act_val *= tmp_value;
+				act_val *= *it_v_op % modulo;
 		}
-
 		it_op++;
 	}
 	if (act_val % modulo == 0)
